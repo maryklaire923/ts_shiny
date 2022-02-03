e9fd7bfd18183dc29e0805310cb4594242edff1e
maryklaire923 <maryklaireosborne@gmail.com>
2022-02-03 09:27:20 -0500

    Adding dates

--- server.R
+++ server.R
@@ -8,14 +8,15 @@ sales_df <- as_tsibble(sales_df, key = c("scode", "pcode"), index = "date")
 
 server <- function(input, output) {
   output$debug <- renderPrint({
-    input$selected_store
-    input$selected_sku
-    sales_df[
-      sales_df$scode == input$selected_store & sales_df$pcode == input$selected_sku, ]
+input$selected_date_range
     })
   output$ts_plot <- renderPlot({
+    min_date <- input$selected_date_range[1]
+    max_date <- input$selected_date_range[2]
     plot_df <- sales_df[
       sales_df$scode == input$selected_store & sales_df$pcode == input$selected_sku, ]
+    plot_df <- plot_df[plot_df$date >= min_date, ]
+    plot_df <- plot_df[plot_df$date <= max_date, ]
     autoplot(plot_df)
   })
 }
--- ui.R
+++ ui.R
@@ -5,6 +5,9 @@ ui <- fluidPage(
               choices = unique(sales_df$scode)),
   selectInput(inputId = 'selected_sku', label = 'Select sku:',
               choices = unique(sales_df$pcode)),
+  dateRangeInput(inputId = 'selected_date_range', label = 'Select date range:',
+                 min = min(sales_df$date), max = max(sales_df$date),
+                 start = min(sales_df$date), end = max(sales_df$date)),
   plotOutput("ts_plot"),
   verbatimTextOutput('debug')
 )

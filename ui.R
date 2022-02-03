25327b67d2e5229a8e4875fe9dbb39c63d6ddd47
maryklaire923 <maryklaireosborne@gmail.com>
2022-02-03 09:15:21 -0500

    App

--- ui.R
+++ ui.R
@@ -1,5 +1,10 @@
 library(shiny)
 
 ui <- fluidPage(
-
+  selectInput(inputId = 'selected_store', label = 'Select store:',
+              choices = unique(sales_df$scode)),
+  selectInput(inputId = 'selected_sku', label = 'Select sku:',
+              choices = unique(sales_df$pcode)),
+  plotOutput("ts_plot"),
+  verbatimTextOutput('debug')
 )
